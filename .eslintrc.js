92118d6eeb5f0645dd36e934cc0c728a7d8af7f9
Kebede <johnzeeze@yahoo.com>
2025-07-16 02:40:55 -0700

    Intial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+
+const _id = "2a";
+console.log(_id);
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": ["warn", { allow: ["error"] }],
+     "no-underscore-dangle": "off",
+  }
+};
