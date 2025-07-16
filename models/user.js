7a88618860316744f44dc06dba01ad73afed2f1b
Kebede <johnzeeze@yahoo.com>
2025-07-16 11:10:39 -0700

    docs: create schemas and models

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+  validator(imageUrl) {
+    return validator.isURL(imageUrl);
+  },
+  message: 'You must enter a valid URL',
+}
+  }
+});
+mongoose.exports = mongoose.model("user", userSchema);
